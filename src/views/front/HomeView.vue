04e579676150c9b3df17cf738375cdf16eaf0fde
lee-nowadays <qwer7983646@gmail.com>
2022-07-20 15:48:37 +0800

    update

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -4,17 +4,18 @@
     v-col(v-if='products.length > 0' cols='12' md='6' lg='3' v-for='product in products' :key='product._id')
       ProductCard(:product='product')
     v-col(v-else cols='12')
-      h1.text-center 沒有商品
+      h1.text-center(v-if="loaded") 沒有商品
+      h1.text-center(v-else indeterminate color="primary") 載入中
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import Swal from 'sweetalert2'
 import { api } from '@/plugins/axios'
 import ProductCard from '@/components/ProductCard.vue'
 
 const products = reactive([])
-
+const loaded = ref(false)
 const init = async () => {
   try {
     const { data } = await api.get('/products')
@@ -26,6 +27,7 @@ const init = async () => {
       text: '伺服器錯誤'
     })
   }
+  loaded.value = true
 }
 init()
 </script>
